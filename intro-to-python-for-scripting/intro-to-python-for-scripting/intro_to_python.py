dee7d9c721ecdfb817f9a1be1112a014185039e7
josephlw7 <vision4thefuture@outlook.com>
2025-06-04 18:27:39 -0700

    commit message

---
+++ intro-to-python-for-scripting/intro-to-python-for-scripting/intro_to_python.py
@@ -0,0 +1,10 @@
+print(type("Hello, World!"))
+# Prints: <class 'str'>
+print(type('Python is awesome'))
+# Prints: <class 'str'>
+print(type(""))
+# Prints: <class 'str'>
+print(type('42'))
+# Prints: <class 'str'>
+print(type(True))
+# Prints: <class 'bool'>
